fbcaf2e9fad418edde556dfad632212a1ba69a55
Thomas-474 <teg2004@icloud.com>
2021-10-19 10:45:07 -0400

    Add project files.

---
+++ CS Inheritance Example/IAutomobile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Inheritance_Example
+{
+    interface IAutomobile
+    {
+        string LicensePlate {get;}
+        double Speed {get;}
+        int Wheels {get;}
+        void Honk();
+    }
+}
+
+
+
+
---
+++ CS Inheritance Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Inheritance_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sedan s = new Sedan(60);
+            s.SpeedUp();
+            Console.WriteLine(s.Describe());
+
+            Truck t = new Truck(45, 500);
+            Console.WriteLine(t.Describe());
+
+            Bicycle b = new Bicycle(10);
+            Console.WriteLine(b.Describe());
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
